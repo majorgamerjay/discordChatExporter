10d8e3a9370a1c63d4a289f6f38ecc8bac40fda4
majorgamerjay <majorgamerjay@protonmail.com>
2021-04-02 15:28:44 +0600

    Update discord.js version and better formatting People get raged at 16 character tabs. So I reformatted it to 4 character tabs. I hope it's okay now :^)

--- src/index.js
+++ src/index.js
@@ -12,111 +12,111 @@ const config = require('./config.json');
 const channelId = config.channelID;
 
 if (config.export_type == "json") {
-		console.log(`Export type: JSON`);
-		var messageHistory = new Array();
+	console.log(`Export type: JSON`);
+	var messageHistory = new Array();
 }
 
 else {
-		console.log(`Export type: md`);
-		var messageHistory = '';
+	console.log(`Export type: md`);
+	var messageHistory = '';
 }
 
 let recursionMeter = 0;
 
 class AuthorStructureInJSON {
-		constructor(message) {
-				this.name = `${message.author.username}#${message.author.discriminator}`;
-				this.avatar = message.author.displayAvatarURL();
-		}
+	constructor(message) {
+		this.name = `${message.author.username}#${message.author.discriminator}`;
+		this.avatar = message.author.displayAvatarURL();
+	}
 }
 
 class MessageStructureInJSON {
-		constructor(message) {
-				this.author = new AuthorStructureInJSON(message);
-				this.content = message.content;
+	constructor(message) {
+		this.author = new AuthorStructureInJSON(message);
+		this.content = message.content;
 
-				this.time = new Date(message.createdTimestamp);
+		this.time = new Date(message.createdTimestamp);
 
-				this.attachments = new Array();
+		this.attachments = new Array();
 
-				// Something to be noticed: in Collection.each(), the roles of
-				// keys and values are reversed where as it would be the same
-				// in other cases.
-				message.attachments.each((key, value) => {
-						this.attachments.push(key.url);
-				});
-		}
+		// Something to be noticed: in Collection.each(), the roles of
+		// keys and values are reversed where as it would be the same
+		// in other cases.
+		message.attachments.each((key, value) => {
+			this.attachments.push(key.url);
+		});
+	}
 }
 
 // Convert message into markdown
 // mS means messageStructure and its kinda hard to retype everytime lol ;p
 function convertMessageStructureIntoMarkdown(mS) {
-		let convertedMessage = `### ${mS.author.name}\n`+
-				`<img src="${mS.author.avatar}" width="128" height="128">\n\n`+
-				`##### ${mS.time}\n\n`+
-				`${mS.content}\n\n`+
-				`Attachments: \n\n`;
+	let convertedMessage = `### ${mS.author.name}\n`+
+		`<img src="${mS.author.avatar}" width="128" height="128">\n\n`+
+		`##### ${mS.time}\n\n`+
+		`${mS.content}\n\n`+
+		`Attachments: \n\n`;
 
-		mS.attachments.forEach(perAttachment => {
-				convertedMessage += `<img src="${perAttachment}" height="400" width="400">\n`;
-		});
+	mS.attachments.forEach(perAttachment => {
+		convertedMessage += `<img src="${perAttachment}" height="400" width="400">\n`;
+	});
 
-		return convertedMessage+'***';
+	return convertedMessage+'***';
 }
 
 // Get message ID for the first time
 function getLastMessageId(channelId) {
-		Bot.channels.cache.get(channelId).messages.fetch({ limit: 1 })
+	Bot.channels.cache.get(channelId).messages.fetch({ limit: 1 })
 		.then(message => message.values().next().value.id);
 }
 
 function printAllMessages(channelId, before) {
-		Bot.channels.cache.get(channelId).messages.fetch({
-				limit: 100,
-				before: before
-		})
+	Bot.channels.cache.get(channelId).messages.fetch({
+		limit: 100,
+		before: before
+	})
 		.then(message => {
-				let nextMessageID = '';
+			let nextMessageID = '';
 
-				// Push to the array containing chat history
-				message.each((key, value) => {
-						// console.log(`${key.content} | ${key.author.username}#${key.author.discriminator} | ${key.id}`);
-						const currentMessage = new MessageStructureInJSON(key);
+			// Push to the array containing chat history
+			message.each((key, value) => {
+				// console.log(`${key.content} | ${key.author.username}#${key.author.discriminator} | ${key.id}`);
+				const currentMessage = new MessageStructureInJSON(key);
 
-						if (config.export_type == "json")
-								messageHistory.push(currentMessage);
-						else if (config.export_type == "md")
-								messageHistory += convertMessageStructureIntoMarkdown(currentMessage) + '\n';
+				if (config.export_type == "json")
+					messageHistory.push(currentMessage);
+				else if (config.export_type == "md")
+					messageHistory += convertMessageStructureIntoMarkdown(currentMessage) + '\n';
 
-						nextMessageID = key.id;
-				});
+				nextMessageID = key.id;
+			});
 
-				// Do recursion
-				if (nextMessageID != '') {
-						++recursionMeter;
-						console.log((recursionMeter*100), 'messages have been fetched');
-						printAllMessages(channelId, nextMessageID);
-				}
+			// Do recursion
+			if (nextMessageID != '') {
+				++recursionMeter;
+				console.log((recursionMeter*100), 'messages have been fetched');
+				printAllMessages(channelId, nextMessageID);
+			}
 
-				// Write to file
-				else {
-						fs.writeFile(config.export_to, config.export_type === "json" ?
-								JSON.stringify(messageHistory,
-								null, 4) : messageHistory, (err) => {
+			// Write to file
+			else {
+				fs.writeFile(config.export_to, config.export_type === "json" ?
+					JSON.stringify(messageHistory,
+						null, 4) : messageHistory, (err) => {
 
-								if (err)
-										throw err;
+							if (err)
+								throw err;
 
-								console.log('Messages are saved!');
+							console.log('Messages are saved!');
 						});
-				}
+			}
 		})
 		.catch(err => console.log(err));
 }
 
 Bot.on('ready', () => {
-		console.log('Bot is ready for exporting messages!');
-		printAllMessages(channelId, getLastMessageId(channelId));
+	console.log('Bot is ready for exporting messages!');
+	printAllMessages(channelId, getLastMessageId(channelId));
 });
 
 Bot.login(config.token);
