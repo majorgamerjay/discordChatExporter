e50afd80ae306b4a4d1eb15b063e9db79e7cc694
majorgamerjay <majorgamerjay@protonmail.com>
2021-04-03 12:59:41 +0600

    Make picture sizes smaller and reasonable

--- src/index.js
+++ src/index.js
@@ -52,13 +52,13 @@ class MessageStructureInJSON {
 // mS means messageStructure and its kinda hard to retype everytime lol ;p
 function convertMessageStructureIntoMarkdown(mS) {
 	let convertedMessage = `### ${mS.author.name}\n`+
-		`<img src="${mS.author.avatar}" width="128" height="128">\n\n`+
+		`<img src="${mS.author.avatar}" width="40" height="40">\n\n`+
 		`##### ${mS.time}\n\n`+
 		`${mS.content}\n\n`+
 		`Attachments: \n\n`;
 
 	mS.attachments.forEach(perAttachment => {
-		convertedMessage += `<img src="${perAttachment}" height="400" width="400">\n`;
+		convertedMessage += `<img src="${perAttachment}" width="300">\n`;
 	});
 
 	return convertedMessage+'***';
