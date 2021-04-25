481a619354720498664e90b3dd588c8a95028cb7
majorgamerjay <majorgamerjay@protonmail.com>
2021-04-25 07:35:37 +0600

    Reindent index.js with softtabs so it will not trigger people

--- src/index.js
+++ src/index.js
@@ -18,14 +18,14 @@ const config = require('./config.json');
 const channelId = config.channelID;
 
 if (config.export_type == "json") {
-	console.log(`Export type: JSON`);
-	var messageHistory = new Array(); // I don't like "var"s but I had to
+    console.log(`Export type: JSON`);
+    var messageHistory = new Array(); // I don't like "var"s but I had to
 }
 
 else {
-	config.export_type == "md" ? console.log(`Export type: md`)
-	: console.log(`Export type: HTML`);
-	var messageHistory = '';
+    config.export_type == "md" ? console.log(`Export type: md`)
+        : console.log(`Export type: HTML`);
+    var messageHistory = '';
 }
 
 /* Counting recursions */
@@ -33,60 +33,60 @@ let recursionMeter = 0;
 
 /* Get message id for the first time */
 function getLastMessageId(channelId) {
-	Bot.channels.cache.get(channelId).messages.fetch({ limit: 1 })
-		.then(message => message.values().next().value.id);
+    Bot.channels.cache.get(channelId).messages.fetch({ limit: 1 })
+        .then(message => message.values().next().value.id);
 }
 
 /* Main recursive function that will do the task */
 function printAllMessages(channelId, before) {
-	Bot.channels.cache.get(channelId).messages.fetch({
-		limit: 100,
-		before: before
-	})
-		.then(message => {
-			let nextMessageID = '';
+    Bot.channels.cache.get(channelId).messages.fetch({
+        limit: 100,
+        before: before
+    })
+        .then(message => {
+            let nextMessageID = '';
 
-			/* Push to the array containing chat history */
-			message.each((key, value) => {
-				// console.log(`${key.content} | ${key.author.username}#${key.author.discriminator} | ${key.id}`);
-				const currentMessage = new Structures.MessageStructureInJSON(key);
+            /* Push to the array containing chat history */
+            message.each((key, value) => {
+                // console.log(`${key.content} | ${key.author.username}#${key.author.discriminator} | ${key.id}`);
+                const currentMessage = new Structures.MessageStructureInJSON(key);
 
-				if (config.export_type == "json")
-					messageHistory.push(currentMessage);
-				else if (config.export_type == "md")
-					messageHistory += Converters.convertMessageStructureIntoMarkdown(currentMessage) +
-						'\n';
+                if (config.export_type == "json")
+                    messageHistory.push(currentMessage);
+                else if (config.export_type == "md")
+                    messageHistory += Converters.convertMessageStructureIntoMarkdown(currentMessage) +
+                        '\n';
 
-				nextMessageID = key.id;
-			});
+                nextMessageID = key.id;
+            });
 
-			/* Do recursion */
-			if (nextMessageID != '') {
-				++recursionMeter;
-				console.log((recursionMeter*100), 'messages have been fetched');
-				printAllMessages(channelId, nextMessageID);
-			}
+            /* Do recursion */
+            if (nextMessageID != '') {
+                ++recursionMeter;
+                console.log((recursionMeter*100), 'messages have been fetched');
+                printAllMessages(channelId, nextMessageID);
+            }
 
-			/* Write to file */
-			else {
-				fs.writeFile(config.export_to, config.export_type === "json" ?
-					JSON.stringify(messageHistory,
-						null, 4) : messageHistory, (err) => {
+            /* Write to file */
+            else {
+                fs.writeFile(config.export_to, config.export_type === "json" ?
+                    JSON.stringify(messageHistory,
+                        null, 4) : messageHistory, (err) => {
 
-							if (err)
-								throw err;
+                            if (err)
+                                throw err;
 
-							console.log('Messages are saved!');
-						});
-			}
-		})
-		.catch(err => console.log(err));
+                            console.log('Messages are saved!');
+                        });
+            }
+        })
+        .catch(err => console.log(err));
 }
 
 /* Triggers when the client is ready to roll */
 Bot.on('ready', () => {
-	console.log('Bot is ready for exporting messages!');
-	printAllMessages(channelId, getLastMessageId(channelId));
+    console.log('Bot is ready for exporting messages!');
+    printAllMessages(channelId, getLastMessageId(channelId));
 });
 
 Bot.login(config.token);
