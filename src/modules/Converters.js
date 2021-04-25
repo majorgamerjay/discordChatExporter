d21107a395527031da81835344cdefd960a16c99
majorgamerjay <majorgamerjay@protonmail.com>
2021-04-25 07:44:24 +0600

    Reindent Converters.js, Structures.js to use softtabs

--- src/modules/Converters.js
+++ src/modules/Converters.js
@@ -2,19 +2,19 @@
 // Convert message into markdown
 // mS means messageStructure and its kinda hard to retype everytime lol ;p
 function convertMessageStructureIntoMarkdown(mS) {
-	let convertedMessage = `### ${mS.author.name}\n`+
-		`<img src="${mS.author.avatar}" width="40" height="40">\n\n`+
-		`##### ${mS.time}\n\n`+
-		`${mS.content}\n\n`+
-		`Attachments: \n\n`;
+    let convertedMessage = `### ${mS.author.name}\n`+
+        `<img src="${mS.author.avatar}" width="40" height="40">\n\n`+
+        `##### ${mS.time}\n\n`+
+        `${mS.content}\n\n`+
+        `Attachments: \n\n`;
 
-	mS.attachments.forEach(perAttachment => {
-		convertedMessage += `<img src="${perAttachment}" width="300">\n`;
-	});
+    mS.attachments.forEach(perAttachment => {
+        convertedMessage += `<img src="${perAttachment}" width="300">\n`;
+    });
 
-	return convertedMessage+'***';
+    return convertedMessage+'***';
 }
 
 module.exports = {
-	convertMessageStructureIntoMarkdown: convertMessageStructureIntoMarkdown
+    convertMessageStructureIntoMarkdown: convertMessageStructureIntoMarkdown
 };
--- src/modules/Structures.js
+++ src/modules/Structures.js
@@ -1,31 +1,31 @@
 class AuthorStructureInJSON {
-	constructor(message) {
-		this.name = `${message.author.username}#${message.author.discriminator}`;
-		this.avatar = message.author.displayAvatarURL();
-	}
+    constructor(message) {
+        this.name = `${message.author.username}#${message.author.discriminator}`;
+        this.avatar = message.author.displayAvatarURL();
+    }
 }
 
 class MessageStructureInJSON {
-	constructor(message) {
-		this.author = new AuthorStructureInJSON(message);
-		this.content = message.content;
+    constructor(message) {
+        this.author = new AuthorStructureInJSON(message);
+        this.content = message.content;
 
-		this.time = new Date(message.createdTimestamp);
+        this.time = new Date(message.createdTimestamp);
 
-		this.attachments = new Array();
-		this.embeds = new Array();
+        this.attachments = new Array();
+        this.embeds = new Array();
 
-		message.attachments.each((key, value) => {
-			this.attachments.push(key.url);
-		});
+        message.attachments.each((key, value) => {
+            this.attachments.push(key.url);
+        });
 
-		message.embeds.forEach(perEmbed => {
-			this.embeds.push(perEmbed);
-		});
-	}
+        message.embeds.forEach(perEmbed => {
+            this.embeds.push(perEmbed);
+        });
+    }
 }
 
 module.exports = {
-	AuthorStructureInJSON: AuthorStructureInJSON,
-	MessageStructureInJSON: MessageStructureInJSON
+    AuthorStructureInJSON: AuthorStructureInJSON,
+    MessageStructureInJSON: MessageStructureInJSON
 };
